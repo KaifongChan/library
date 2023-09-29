c253d23993338c6efb4980ea8f3342f207a2870b
Kaifong Chan <143081680+KaifongChan@users.noreply.github.com>
2023-09-30 00:28:51 +1000

    Fix delete button linked with book logs, created new divs to accomodate this change

--- script.jsx
+++ script.jsx
@@ -11,19 +11,19 @@ function Book(title, author, completed) {
     return;
 }
 
-const entry_1 = new Book("A", "B", false);
-const entry_2 = new Book("C", "D", true);
+// const entry_1 = new Book("A", "B", false);
+// const entry_2 = new Book("C", "D", true);
 
-myLibrary.push(entry_1);
-myLibrary.push(entry_2);
+// myLibrary.push(entry_1);
+// myLibrary.push(entry_2);
 
-let container = document.getElementById("show-content");
-for (let i = 0; i < myLibrary.length; i++) {
-    let div = document.createElement("div");
-    div.classList.add("book-card");
-    div.textContent = `${i + 1}. The book is ${myLibrary[i].title} written by ${myLibrary[i].author}. It ${(myLibrary[i].completed ? `has been read.` : `has not been read yet.`)}`;
-    container.appendChild(div);
-}
+// let container = document.getElementById("show-content");
+// for (let i = 0; i < myLibrary.length; i++) {
+//     let div = document.createElement("div");
+//     div.classList.add("book-card");
+//     div.textContent = `${i + 1}. The book is ${myLibrary[i].title} written by ${myLibrary[i].author}. It ${(myLibrary[i].completed ? `has been read.` : `has not been read yet.`)}`;
+//     container.appendChild(div);
+// }
 
 // console.log(entry_1.info());
 // console.log(entry_2.info());
@@ -66,43 +66,83 @@ function validateSubmission(book, author) {
     }
 }
 
+let book_list_container = document.getElementById("show-book");
+let button_container = document.getElementById("delete-book");
+
 function addBookToLibrary(title, author, status) {
+
+    // creates a book object
     const newBook = new Book(title, author, status);
+
+    //appends into library array
     myLibrary.push(newBook);
-    let containerDiv = document.createElement("div");
+
+    //create cardDiv
     let cardDiv = document.createElement("div");
     cardDiv.classList.add("book-card");
     cardDiv.classList.add(`${title}`)
     cardDiv.textContent = `${myLibrary.length}. The book is ${title} written by ${author}. It ${(status === "read" ? `has been read.` : `has not been read.`)}`
-    containerDiv.appendChild(cardDiv);
-    const newBtn = document.createElement("button");
-    newBtn.value = `${title}`
-    // console.log("HELL")
-    newBtn.addEventListener('click', function (e) {
-        removeBook(myLibrary, newBtn.value);
+    book_list_container.appendChild(cardDiv);
+
+
+    //create button linked with cardDiv
+    const newButton = document.createElement("button");
+    newButton.textContent = "Delete"
+    newButton.value = `${title}`
+    newButton.addEventListener('click', function (e) {
+        // removes book from myLibrary linked with the button
+        removeBook(myLibrary, newButton.value);
     })
-    containerDiv.appendChild(newBtn);
-    container.appendChild(containerDiv);
+    button_container.appendChild(newButton);
+    return;
 }
 
 function removeBook(source, interest) {
-    for (let x = 0; x < source.length; x++) {
+
+    // delete book from library
+    for (let x = 0 ; x < source.length; x ++ ) {
         if (source[x].title === interest) {
             myLibrary.splice(x, 1);
         }
     }
-    let findDiv = container.children;
-    console.log(findDiv);
-    let findDivTwice = findDiv.children;
-    for (let x = 0; x < findDivTwice.length; x++) {
-        if (findDivTwice[x].classList.contains(interest)) {
-            findDivTwice[x].remove();
+    let findDiv = book_list_container.children;
+    let findButton = button_container.children;
+    for (let x = 0; x < findDiv.length; x ++ ) {
+        if (findDiv[x].classList.contains(interest)) {
+            findDiv[x].remove()
         }
-        if (findDivTwice[x].value === interest) {
-            findDivTwice.remove();
+        if (findButton[x].value === interest) {
+            findButton[x].remove();
         }
     }
+    return;
 }
+
+function reset() {
+    book_list_container.innerHTML = "";
+    button_container.innerHTML = "";
+    return;
+}
+
+
+// function removeBook(source, interest) {
+//     for (let x = 0; x < source.length; x++) {
+//         if (source[x].title === interest) {
+//             myLibrary.splice(x, 1);
+//         }
+//     }
+//     let findDiv = container.children;
+//     console.log(findDiv);
+//     let findDivTwice = findDiv.children;
+//     for (let x = 0; x < findDivTwice.length; x++) {
+//         if (findDivTwice[x].classList.contains(interest)) {
+//             findDivTwice[x].remove();
+//         }
+//         if (findDivTwice[x].value === interest) {
+//             findDivTwice.remove();
+//         }
+//     }
+// }
 
 
 // function removeBook(source, interest) {
@@ -116,6 +156,6 @@ function removeBook(source, interest) {
 // document.getele
 
 
-function reset() {
-    container.innerHTML = "";
-}
+// function reset() {
+//     container.innerHTML = "";
+// }
